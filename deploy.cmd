0830a3e64a5e776018768be7fff528224954f64c
banqinghe <qingheban@qq.com>
2021-11-20 22:51:36 +0800

    chore: update deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,15 @@
 @echo off
-npm run build
 cd dist
+
+if exist .git goto :update_repo
+
 git init
 git remote add origin git@github.com:banqinghe/banqinghe.github.io.git
 git checkout --orphan gh-pages
+
+:update_repo
 git add -A
 git commit -m "deploy"
 git push -f origin gh-pages
+
+cd..
